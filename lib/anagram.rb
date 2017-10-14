e027a53f80e718fb93ac72f7a620221efc7bada1
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-14 16:15:22 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :match
+
+  def initialize(word)
+    anagram = self.new
+    anagram.match = word
+    anagram
+  end
+
+  def match(string)
+    got_it= ""
+    string.split("").sort.each do |string_w|
+      if (string_w.split("").sort == match.split("").sort)
+        got_it=string_w
+      end
+    end
+    got_it
+  end
+
+end
