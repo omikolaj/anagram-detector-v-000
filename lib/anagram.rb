d4644cd1ff5bc2ef4720156e2c0daa4df848bffe
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-14 17:13:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    self
+  end
+
+  def match(string)
+    match = []
+    string.each do |item|
+      if (item.split("").sort == @word.split("").sort)
+        match << item
+      end
+    end
+    match
+  end
+end
